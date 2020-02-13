18e96669297137de85db47a6f7ad714c2ac05e8f
unknown <javimoya11@gmail.com>
2020-02-13 11:43:51 +0100

    se ve un gato

--- js/app.js
+++ js/app.js
@@ -1,12 +1,40 @@
-const axios = require('axios');
+const postGet = 'https://api.thecatapi.com/v1/images/search';
 
-axios.get('http://localhost:3000/db')
-    .then(resp => {
-        data = resp.data;
-        data.forEach(e => {
-            console.log(`${e.first_name}, ${e.last_name}, ${e.email}`);
-        });
+function request(url) {
+    return new Promise(function (resolve, reject) {
+        const xhr = new XMLHttpRequest();
+        xhr.timeout = 2000;
+        xhr.onreadystatechange = function (e) {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(xhr.response);
+                } else {
+                    reject(xhr.status);
+                }
+            }
+        };
+        xhr.ontimeout = function () {
+            reject('timeout')
+        };
+        xhr.open('get', url, true);
+        xhr.send();
+    });
+}
+
+
+
+
+const myPromise = request(postGet);
+console.log('will be pending when logged', myPromise)
+
+myPromise
+    .then(function imprimirPosts(json) {
+        console.log('when resolve is found it comes here with the response, in this case posts ')
+
+        const listGato = JSON.parse(json);
+        listGato.forEach(gato => document.getElementById("gato").innerHTML = "<img src="+gato.url+">");
+
     })
-    .catch(error => {
-        console.log(error);
-    });    
+    .catch(function handleErrors(error) {
+        console.log('when a reject is executed it will come here ignoring the then statement ', error)
+    })
