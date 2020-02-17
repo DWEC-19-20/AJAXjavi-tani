362a8dcf4829e4e48e64f7a20d3508586aa7a26c
Dannerino <tarumendentany@gmail.com>
2020-02-17 11:29:50 +0100

    añadidas imágenes por categorías

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const categorias = 'https://api.thecatapi.com/v1/categories';
-var catID; //Esto lo pongo así aquí para acordarme que tengo que meterlo luego en una función y añadirselo a la url de abajo, la de la constante imágenes
-const imagenes = 'https://api.thecatapi.com/v1/images/search?size=9&category_ids='+catID;
+var catID=document.getElementById("gato").value; //Esto lo pongo así aquí para acordarme que tengo que meterlo luego en una función y añadirselo a la url de abajo, la de la constante imágenes
+var imagenes = 'https://api.thecatapi.com/v1/images/search?limit=3&category_ids='+catID;
+
 
 function request(url) {
     return new Promise(function (resolve, reject) {
@@ -23,7 +24,26 @@ function request(url) {
     });
 }
 
-
+document.getElementById("gato").addEventListener("change", ()=>{
+   
+    const myPromise2 = request(imagenes);
+    console.log('will be pending when logged', myPromise)
+    
+    myPromise2
+        .then(function imprimirPosts(json) {
+            console.log('when resolve is found it comes here with the response, in this case posts ')
+    
+            const listGato = JSON.parse(json);
+            console.log(listGato);
+            document.getElementById("gatos").innerHTML="";
+            listGato.forEach(gato => document.getElementById("gatos").innerHTML += "<img src="+gato.url+"><br>");
+    
+        })
+        .catch(function handleErrors(error) {
+            console.log('when a reject is executed it will come here ignoring the then statement ', error)
+        })
+    
+});
 
 
 const myPromise = request(categorias);
@@ -34,9 +54,10 @@ myPromise
         console.log('when resolve is found it comes here with the response, in this case posts ')
 
         const listGato = JSON.parse(json);
-        listGato.forEach(gato => document.getElementById("gato").innerHTML += "<option value="+gato.name+">"+gato.name+"</option>");
+        listGato.forEach(gato => document.getElementById("gato").innerHTML += "<option value="+gato.id+">"+gato.name+"</option>");
 
     })
     .catch(function handleErrors(error) {
         console.log('when a reject is executed it will come here ignoring the then statement ', error)
     })
+
