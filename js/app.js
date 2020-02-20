1d5c0553b954d0d14a40a5cd8d7aecdfa5df24a8
Dannerino <tarumendentany@gmail.com>
2020-02-20 12:47:39 +0100

    url cambia, mejorar resta catID

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 const categorias = 'https://api.thecatapi.com/v1/categories';
-var catID=document.getElementById("gato").value; //Esto lo pongo así aquí para acordarme que tengo que meterlo luego en una función y añadirselo a la url de abajo, la de la constante imágenes
-var imagenes = 'https://api.thecatapi.com/v1/images/search?limit=3&category_ids='+catID;
+var imagenes = 'https://api.thecatapi.com/v1/images/search?limit=10&category_ids=';
 
 
 function request(url) {
@@ -24,27 +23,38 @@ function request(url) {
     });
 }
 
-document.getElementById("gato").addEventListener("change", ()=>{
+document.getElementById("2").addEventListener("click", () => {
+    pagina = 2;
+});
+document.getElementById("3").addEventListener("click", () => {
+    pagina = 3;
+});
+
+
+
+document.getElementById("gato").addEventListener("change", () => {
    
+    catID = document.getElementById("gato").value;
+    imagenes=imagenes+catID;
+    console.log("ESTA ES LA URL"+imagenes);
     const myPromise2 = request(imagenes);
-    console.log('will be pending when logged', myPromise)
     
+    
+    console.log('will be pending when logged', myPromise2)
     myPromise2
         .then(function imprimirPosts(json) {
             console.log('when resolve is found it comes here with the response, in this case posts ')
-    
+
             const listGato = JSON.parse(json);
-            console.log(listGato);
-            document.getElementById("gatos").innerHTML="";
-            listGato.forEach(gato => document.getElementById("gatos").innerHTML += "<img src="+gato.url+"><br>");
-    
+        console.log(listGato);
+            document.getElementById("gatos").innerHTML = "";
+            listGato.forEach(gato => document.getElementById("gatos").innerHTML += "<img src=" + gato.url + "><br>");
+
         })
         .catch(function handleErrors(error) {
             console.log('when a reject is executed it will come here ignoring the then statement ', error)
         })
-    
 });
-
 
 const myPromise = request(categorias);
 console.log('will be pending when logged', myPromise)
@@ -52,11 +62,14 @@ console.log('will be pending when logged', myPromise)
 myPromise
     .then(function imprimirPosts(json) {
         console.log('when resolve is found it comes here with the response, in this case posts ')
-
         const listGato = JSON.parse(json);
-        listGato.forEach(gato => document.getElementById("gato").innerHTML += "<option value="+gato.id+">"+gato.name+"</option>");
+        listGato.forEach(gato => document.getElementById("gato").innerHTML += "<option value=" + gato.id + ">" + gato.name + "</option>");
 
+        var iu=document.getElementById("gato");
+        for (let i =0; i<iu.length;i++){
+        console.log(iu[i]);}
     })
+
     .catch(function handleErrors(error) {
         console.log('when a reject is executed it will come here ignoring the then statement ', error)
     })
